a59d378f85c8a80705372af12342b737a9be7dae
m23ck <andojomack@gmail.com>
2020-01-29 09:47:41 -0300

    last updates

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var cors = require('cors')
 const express = require("express");
 const app = express();
 const gebruikerRouter = require("./api/gebruikers/gebruiker.router");
@@ -6,18 +7,17 @@ const typeRouter = require("./api/type/type.router");
 const ressortRouter = require("./api/ressort/ressort.router");
 const presentieRouter = require("./api/presentie/presentie.router");
 
+//Remove when in production
 
+app.use(cors())
 
 app.use(express.json());
-
 
 app.use("/api/gebruikers", gebruikerRouter);
 app.use("/api/type", typeRouter);
 app.use("/api/ressort", ressortRouter);
 app.use("/api/presentie", presentieRouter);
 
-
-
-app.listen(process.env.APP_PORT, ()=>{
-    console.log("Server running on port", process.env.APP_PORT);
-})
+app.listen(process.env.APP_PORT, () => {
+  console.log("Server running on port", process.env.APP_PORT);
+});
