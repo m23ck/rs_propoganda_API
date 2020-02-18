aefe1f6f439bc91e8e6ce58c2c4c68befc0e1049
m23ck <andojomack@gmail.com>
2020-02-18 08:30:47 -0300

    added expiresin function to the token and smade it 10h

--- api/gebruikers/gebruiker.controller.js
+++ api/gebruikers/gebruiker.controller.js
@@ -149,7 +149,7 @@ module.exports = {
                 const jsonToken = sign({
                     result: results
                 }, process.env.KEY, {
-                    expiresIn: "3h"
+                    expiresIn: "10h"
                 });
                 return res.status(200).json({
                     success: 1,
